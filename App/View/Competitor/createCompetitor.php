cafef3ee66148d000dcc5d42c3eb58a4e77875db
giovannagabi <giovannagabrielly2302@gmail.com>
2024-09-06 11:21:49 -0300

    gigas

--- App/View/Competitor/createCompetitor.php
+++ App/View/Competitor/createCompetitor.php
@@ -24,19 +24,65 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="Public/Css/create.css">
+    <link rel="stylesheet" href="../../../Public/Css/header.css">
+    <link rel="stylesheet" href="../../../Public/Css/create.css">
+    
     <title>View teste</title>
 </head>
 <body>
-<form method="post">
+<div class="header">
+    <script src="../../../Public/Assets/js/script.js"></script>
+    <div id="menu">
+  <div id="menu-bar" onclick="menuOnClick()">
+    <div id="bar1" class="bar"></div>
+    <div id="bar2" class="bar"></div>
+    <div id="bar3" class="bar"></div>
+  </div>
+  <nav class="nav" id="nav">
+    <ul class="ulheader">
+      <li class="liheader"><a class="op" href="../../../App/View/Sport/createSport.php">Esporte</a></li>
+      <li class="liheader"><a class="op" href="../../../App/View/Competitor/createCompetitor.php">Competidor</a></li>
+      <li class="liheader"><a class="op" href="../../../App/View/Trainer/createTrainer.php">Treinador</a></li>
+      <li class="liheader"><a class="op" href="../../../App/View/Locality/createLocality.php">Localidade</a></li>
+    </ul>
+  </nav> 
+</div>
+<div class="menu-bg" id="menu-bg"></div>
+<div class="logo">
+        <img src="../../../Public/Assets/img/logo.jpeg" alt="Logo">
+    </div>
+<div class="icon">
+    <img src="../../../Public/Assets/img/Icon.png">
+</div>
+    </div>
+    <h1 class="title">COMPETIDOR</h1>
+
+<div class="container-cadastro">
+<form action="#" method="POST">
+        <div class="form-group">
         <input type="text" name="name" placeholder="Nome" required>
+        </div>
+        <div class="form-group">
         <input type="text" name="age" placeholder="Idade" required>
+        </div>
+        <div class="form-group">
         <input type="text" name="height" placeholder="Altura" required>
+        </div>
+        <div class="form-group">
         <input type="text" name="gender" placeholder="Sexo" required>
+        </div>
+        <div class="form-group">
         <input type="text" name="CPF" placeholder="CPF" required>
+        </div>
+        <div class="form-group">
         <input type="text" name="RG" placeholder="RG" required>
+        </div>
+        <div class="form-group">
         <input type="text" name="team" placeholder="Equipe" required>
+        </div>
         <button type="submit">Adicionar Competidor</button>
+        <a class="listar" href="Competitor.php">Listar</a>
     </form>
+    </div>
 </body>
 </html>
